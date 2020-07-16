72c012798bd0a4731fa78aaf9d80b031c72057bc
Ruth Poliakon <r.poliakon@gmail.com>
2020-07-16 17:11:40 -0600

    eating_cookes alternative memoization (works with original test file)

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,7 +7,32 @@ import functools
 
 # return the number of ways n cookies can be eaten if cookies can be eaten one, two, or three at a time
 # memoization
+def eating_cookies(n, cache={}):
+    # base cases
+    # 0 ways to eat negative cookies
+    if n < 0:
+        cache[0] = 0
+        return 0
+    # 1 way to eat 0 cookies
+    if n == 0:
+        cache[1] = 1
+        return 1
+
+    if n in cache:
+        return cache[n]
+
+    # recursive case
+    # eating 3 cookies, eating 2 cookies, eating 1 cookie
+    result = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    cache[n] = result
+    return result
+
+
+'''
+# memoization
 @functools.lru_cache()
+
+# first pass solution
 def eating_cookies(n):
     # base cases
     # 0 ways to eat negative cookies
@@ -19,7 +44,7 @@ def eating_cookies(n):
     # recursive case
     # eating 3 cookies, eating 2 cookies, eating 1 cookie
     return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
-
+'''
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
