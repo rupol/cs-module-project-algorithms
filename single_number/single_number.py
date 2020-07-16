c0e4f79edf160b8f857abc627c816bb688fac617
Ruth Poliakon <r.poliakon@gmail.com>
2020-07-16 17:58:54 -0600

    single number optimization

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,9 +2,24 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 # return the integer that only shows up once in the array
+def single_number(arr):
+    result = arr[0]
+
+    # do XOR of all elements
+    for i in range(1, len(arr)):
+        result = result ^ arr[i]
+    return result
+
+# XOR (zor) operator: compares two bits
+# if identical: 0
+# if different: 1
 
 
+'''
+# first pass solution
 def single_number(arr):
     # order the array
     arr.sort()
@@ -20,10 +35,10 @@ def single_number(arr):
             return arr[i]
         # after each pair, i ++ (skip)
         i += 2
-
+'''
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 3, 4, 4, 5, 5, 3, 9, 9, 0, 1]
+    arr = [1, 3, 0, 4, 5, 5, 3, 9, 9, 0, 1]
 
     print(f"The odd-number-out is {single_number(arr)}")
